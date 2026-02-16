43b32cc0b9ccfa487249b380b1fbe2957f62185a
noknownnames <itsn0noohno@gmail.com>
2026-02-16 19:19:00 +0100

    ---

---
+++ PrincipyProgramovani/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Data.Common;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 8;
+            int column = 10;
+            int[] vipRow = [7, 8];
+            int ticketPrice = 180;
+            int vipTicketPrice = 250;
+            SalKina Kino = new SalKina(row,column,vipRow,ticketPrice,vipTicketPrice);
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("-!-|-Kino Rezervace 2026-|-!-\n");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Menu MainMenu = new Menu();
+            while (true)
+            {
+                MainMenu.VypsatMoznosti();
+                string input = Console.ReadLine();
+                MainMenu.RunCommandFromInput(input);
+            }
+        }
+        /*Program musí umožnit:
+        TODO Zobrazit kinosál(řady × sedadla, volné / obsazené)
+        TODO Rezervovat sedadlo/sedadla(jen to které existuje a je volné)
+        done Ověřit, zda je sedadlo volné
+        TODO Spočítat cenu lístku
+        TODO Ukončit program
+        TODO zobrazit hlavní menu akcí
+        */
+        class Menu
+        {
+            public Menu()
+            {
+                string[] CommandNames = new string[5];//{ "V", "R", "F", "C", "/" };
+                CommandNames[0] = "V";
+                CommandNames[0] = "R";
+                CommandNames[0] = "F";
+                CommandNames[0] = "C";
+                CommandNames[0] = "/";
+            }
+            string[] CommandNames { get; set; }
+            public void DefaultColors()
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            public void VypsatMoznosti()
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Dobrý den, chcete...\n[V] Zobrazit kinosál\n[R] Rezervovat sedadlo\n[F] Ověřit, zdali je sedadlo volné\n[C] Spočítat cenu lístku\n[/] Ukončit program\n");
+                DefaultColors();
+            }
+            private bool IsInputValid(string input)
+            {
+                foreach (string command in CommandNames)
+                    if (input == command)
+                    {
+                        return true;
+                    }
+                return false;
+            }
+            public void RunCommandFromInput(string input)
+            {
+            if (!IsInputValid(input))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"akce {input} neexistuje. Zkuste zadat akci, která existuje.\n");
+                    DefaultColors();
+                }
+            }
+        }
+        class SalKina
+        {
+            public SalKina(int rady, int sloupce, int[]? vipRady, int listekCena, int vipListekCena)
+            {
+                bool[,] Sedadla = new bool[sloupce, rady];
+                int PocetRad = rady;
+                int PocetSloupcu = sloupce;
+                int[]? VipRady = vipRady;
+                int ListekCena = listekCena;
+                int VipListekCena = vipListekCena;
+            }
+            public bool[,] Sedadla { get; set; }
+            public int PocetRad { get; set; }
+            public int PocetSloupcu { get; set; }
+            public int[]? VipRady { get; set; }
+            public int ListekCena { get; set; }
+            public int VipListekCena { get; set; }
+
+            public void PrintSalKina()
+            {
+                for (int i = 0; i < PocetSloupcu; i++)
+                {
+                    for (int j = 0; j < PocetRad; i++)
+                    {
+                        Console.Write($"|{Sedadla[i, j]}");
+                    }
+                }
+                Console.WriteLine("|");
+            }
+            public bool isSedadloZabrane(int sloupec, int rada)
+            {
+                return Sedadla[sloupec, rada];
+            }
+        }
+    }
+}
+
---
+++ Topologicke usporadani/Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Topologicke_usporadani
+{
+    internal class Program
+    {
+        static List<int> TopologicalySortVertices(int[,] inputMatrix)
+        {
+            int NumberOfVerts = inputMatrix.GetLength(0);
+            List<int> sortedVertices = new List<int>;
+            char[] verticeStatus = new char[NumberOfVerts];//[o]pen,[u]ndiscovered,[c]losed
+            foreach (int i in sortedVertices)
+                verticeStatus [i] = 'u';
+            foreach (int i in sortedVertices)
+            {
+                if (sortedVertices[i] == 'u')
+                    _dfs(i);
+            }
+            List<int> reversedSortedVertices = new List<int>;
+            for  
+            void _dfs(int vertex)
+            {
+                verticeStatus[vertex] = 'o';
+                for (int i = 0; i < NumberOfVerts;i++)
+                {
+                    if (inputMatrix[vertex,i] == -1)
+                    {
+                        if (verticeStatus[i] == 'o')
+                            throw new Exception("Graf obsahuje cyklus, nelze seřadit!");
+                        else if (verticeStatus[i] == 'u')
+                            _dfs(i);
+                    }
+                    verticeStatus[vertex] = 'c';
+                    sortedVertices.Add(i);
+                }
+            }
+        }
+        static List<char>[] TurnSringInputIntoListOfCharArrays(string input) 
+        {
+            string[] inputArray = input.Split();
+            List<char>[] charArrayOfLists = new List<char>[inputArray.Length];
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                charArrayOfLists[i] = [];
+                char[] charArray = inputArray[i].ToCharArray();
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    charArrayOfLists[i].Add(charArray[j]);
+                }
+            }
+            return charArrayOfLists;
+        }
+        static List<char> SampleEachUniqueCharacter(List<char>[] charArrayOfLists)
+        {
+            List<char> samples = new List<char>();
+            for (int i = 0; i < charArrayOfLists.Length; i++)
+            {
+                foreach (char character in charArrayOfLists[i])
+                {
+                    foreach (char sample in samples)
+                    {
+                        if (character == sample )
+                        {
+                            break;
+                        }
+                        samples.Add(sample);
+                    }
+                }
+            }
+            return samples;
+        }
+        static List<char[]> SampleRelationshipsBetweenCharacters(List<char>[] charArrayOfLists)
+        {
+            List<char[]> earlierToLaterRelationshipsInAlphabet = new List<char[]>();
+            for (int i = 0; i < charArrayOfLists.Length - 1; i++)
+            {
+                int arrayLen = charArrayOfLists[i].Count;
+                int secondArrayLen = charArrayOfLists[i + 1].Count;
+                if (arrayLen > secondArrayLen)
+                {
+                    arrayLen = secondArrayLen;
+                }
+                /*hledáme 2x2 prostor v našem poli listů
+                     A|A B| 
+                     A|A C|
+                     B A D
+                    ve kterém můžeme porovnat, která písmena po sobě musí následovat
+                */
+                for (int j = arrayLen - 1; j > 0; j--)
+                {
+                    if (charArrayOfLists[i][j - 1] == charArrayOfLists[i + 1][j - 1] && charArrayOfLists[i][j] != charArrayOfLists[i + 1][j])
+                    {
+                        foreach (char[] realtionship in earlierToLaterRelationshipsInAlphabet)
+                        {
+                            if (realtionship[0] == charArrayOfLists[i][j] && realtionship[1] == charArrayOfLists[i - 1][j])
+                            {
+                                break;
+                            }
+                            earlierToLaterRelationshipsInAlphabet.Add([Convert.ToChar(charArrayOfLists[i][j]), Convert.ToChar(charArrayOfLists[i - 1][j])]);
+                        }
+                    }
+                }
+                if (charArrayOfLists[i][0] != charArrayOfLists[i + 1][0])//pro případ, kdy porovnáváme první písmeno v řádku
+                {
+                    foreach (char[] realtionship in earlierToLaterRelationshipsInAlphabet)
+                    {
+                        if (realtionship[0] == charArrayOfLists[i][0] && realtionship[1] == charArrayOfLists[i - 1][0])
+                        {
+                            break;
+                        }
+                        earlierToLaterRelationshipsInAlphabet.Add([Convert.ToChar(charArrayOfLists[i][0]), Convert.ToChar(charArrayOfLists[i - 1][0])]);
+                    }
+                }
+            }
+            return earlierToLaterRelationshipsInAlphabet;
+        }
+        static Dictionary<int, char> CreateNumberLetterPairs(List<char>uniqueCharacterList)
+        {
+            Dictionary<int,char> dictionary = new Dictionary<int,char>();
+            for (int i = 0; i < uniqueCharacterList.Count; i++)
+            {
+                dictionary.Add(i, uniqueCharacterList[i]);
+            }
+            return dictionary;
+        }
+        static Dictionary<char, int> CreateReversedNumberLetterPairs(List<char> uniqueCharacterList)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            for (int i = 0; i < uniqueCharacterList.Count; i++)
+            {
+                dictionary.Add(uniqueCharacterList[i],i);
+            }
+            return dictionary;
+        }
+        static int[,] RelationshipDictionaryToIncidenceMatrix(List<char[]> earlierToLaterRelationshipsInAlphabet, int uniqueCharListLength, Dictionary<char, int> nameToNumberLookup)
+        {
+            int[,] incidenceMatrix = new int[uniqueCharListLength,uniqueCharListLength];
+            foreach (char[] letterpair in earlierToLaterRelationshipsInAlphabet)
+            {
+                int letter1 = nameToNumberLookup[letterpair[0]];
+                int letter2 = nameToNumberLookup[letterpair[1]];
+                incidenceMatrix[letter1, letter2] = 1;
+                incidenceMatrix[letter2, letter1] = -1;
+            }
+            return incidenceMatrix;
+        }
+        
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<char>[] charArrayOfLists = TurnSringInputIntoListOfCharArrays(input);
+            List<char> uniqueCharList = SampleEachUniqueCharacter(charArrayOfLists);
+            List<char[]> earlierToLaterCharacterRelationships = SampleRelationshipsBetweenCharacters(charArrayOfLists);
+            Dictionary<int, char> numberToNameLookup = CreateNumberLetterPairs(uniqueCharList);
+            Dictionary<char, int> nameToNumberLookup = CreateReversedNumberLetterPairs(uniqueCharList);
+            int[,] relationshipIncidenceMatrix = RelationshipDictionaryToIncidenceMatrix(earlierToLaterCharacterRelationships, uniqueCharList.Count, nameToNumberLookup);
+        }
+    }
+}
